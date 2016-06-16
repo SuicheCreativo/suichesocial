c4a964be87da6d9aa9b1303f782575e40354b19f
suichecreativo <proyectos@suichecreativo.com>
2016-06-16 17:05:43 -0500

    Escoge donde publicar los botones

--- suichesocial.php
+++ suichesocial.php
@@ -16,7 +16,7 @@ class plgContentSuichesocial extends JPlugin
 		$option = $app->input->getCmd('option', '');
 		$siteName = $config->get('sitename');
 		
-	 	if($option == "com_content"){ 
+	 	if($option == "com_content" && $this->params->get('contenido')==1){ 
 	 		$link = $_SERVER[REQUEST_URI];
 	 		$title = $article->title;
 	 		if($this->params->get('facebook')==1){ $html .= '<a href="https://www.facebook.com/dialog/feed?app_id=145634995501895&display=popup&caption='.$_SERVER[SERVER_NAME].'&link=http://'.$_SERVER[SERVER_NAME].$link.'&p[title]='.$title.'" target="_blank"><img src="'.JUri::root().'plugins/content/suichesocial/img/facebook.png" alt="Compartir en Facebook" style="margin: 0px 2px;"></a>'; };
@@ -24,14 +24,14 @@ class plgContentSuichesocial extends JPlugin
 	 		if($this->params->get('twitter')==1){ $html .= '<a href="https://twitter.com/?status=Quiero compartir contigo algo de '.$siteName.' que me gustó http://'.$_SERVER[SERVER_NAME].$link.'" target="_blank"><img src="'. JUri::root() . 'plugins/content/suichesocial/img/twitter.png" alt="Compartir en twitter"  style="margin: 0px 2px;"/></a>';};
 	 	};
 
-	 	if($option == "com_k2"){ 
+	 	if($option == "com_k2" && $this->params->get('k2')==1){ 
 	 		$link = $article->link;
 	 		$title = $article->title;
 	 		if($this->params->get('facebook')==1){ $html .= '<a href="https://www.facebook.com/dialog/feed?app_id=145634995501895&display=popup&caption='.$_SERVER[SERVER_NAME].'&link=http://'.$_SERVER[SERVER_NAME].$link.'&p[title]='.$title.'" target="_blank"><img src="'.JUri::root().'plugins/content/suichesocial/img/facebook.png" alt="Compartir en Facebook" style="margin: 0px 2px;"></a>'; };
 	 		if($this->params->get('google')==1){ $html .= '<a href="https://plus.google.com/share?url=http://'.$_SERVER[SERVER_NAME].$link.'" target="_blank"><img src="'. JUri::root() . 'plugins/content/suichesocial/img/google.png" alt="Compartir en google plus"  style="margin: 0px 2px;"/></a>'; };
 	 		if($this->params->get('twitter')==1){ $html .= '<a href="https://twitter.com/?status=Quiero compartir contigo algo de '.$siteName.' que me gustó http://'.$_SERVER[SERVER_NAME].$link.'" target="_blank"><img src="'. JUri::root() . 'plugins/content/suichesocial/img/twitter.png" alt="Compartir en twitter"  style="margin: 0px 2px;"/></a>';};
 	 	};		
-
+	 	
 		return $html;
 	}
 }
